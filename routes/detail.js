d73c75a24f8c6d36c70dcae874669bfc3ef054a6
patcharapun sathitsommon <patcharapun.sat@gmail.com>
2023-03-18 01:18:08 +0700

    home, detail

--- routes/detail.js
+++ routes/detail.js
@@ -1,10 +1,29 @@
 const router = require("./router");
+const request = require("request");
 
 router.get("/detail/:id", function (req, res, next) {
-  const paramsend = {
+  let paramsend = {
     views: "detail.ejs",
   };
-  res.render("index.ejs", paramsend);
+  let id = req.params.id;
+  const options = {
+    uri: `https://ton-core.contributiondao.com/telegram/groups/${id}`,
+    method: `GET`,
+    contentType: "application/json",
+    // headers: {
+    //   Authorization: `${stsconfig.authorizationkey}`,
+    // },
+  };
+  request(options, (req, ress) => {
+    let data = ress.body ? ress.body : null;
+    let detail = JSON.parse(data);
+    paramsend = {
+      ...paramsend,
+      detail,
+    };
+    console.log(paramsend);
+    res.render(`index.ejs`, paramsend);
+  });
 });
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,28 @@
 const router = require("./router");
 const detail = require("./detail");
+const request = require("request");
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  const paramsend = {
+  let paramsend = {
     views: "home.ejs",
   };
-  res.render("index.ejs", paramsend);
+  const options = {
+    uri: `https://ton-core.contributiondao.com/telegram/groups`,
+    method: `GET`,
+    contentType: "application/json",
+    // headers: {
+    //   Authorization: `${stsconfig.authorizationkey}`,
+    // },
+  };
+  request(options, (req, ress) => {
+    let data = ress.body ? ress.body : null;
+    let alldata = JSON.parse(data);
+    paramsend = {
+      ...paramsend,
+      alldata,
+    };
+    res.render(`index.ejs`, paramsend);
+  });
 });
 
 module.exports = [router, detail];
