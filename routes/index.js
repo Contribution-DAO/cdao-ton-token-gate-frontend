b0a6d84a623c13660320d37cb25f895279f37666
patcharapun sathitsommon <patcharapun.sat@gmail.com>
2023-03-19 03:06:21 +0700

    group link

--- routes/index.js
+++ routes/index.js
@@ -24,5 +24,46 @@ router.get("/", function (req, res, next) {
     res.render(`index.ejs`, paramsend);
   });
 });
-
+router.post("/grouplink", function (req, res, next) {
+  let token = req.headers.authorization ? req.headers.authorization : null;
+  if (!token) {
+    res.status(500).send({ error: true, msg: "No Token" });
+    return;
+  } else {
+    let { id, inviteLink, twitterUsername } = req.body;
+    if (!id) {
+      res.status(500).send({ error: true, msg: "No id value" });
+      return;
+    } else if (!inviteLink) {
+      res.status(500).send({ error: true, msg: "No Invitation Link value" });
+      return;
+    } else if (!twitterUsername) {
+      res.status(500).send({ error: true, msg: "No Twitter Username value" });
+      return;
+    }
+    let senddata = {
+      id,
+      inviteLink,
+      twitterUsername,
+    };
+    // let senddata = {
+    //   id: "-868694703",
+    //   twitterUsername: "@Optidomains",
+    //   isSecret: false,
+    // };
+    const options = {
+      uri: `https://ton-core.contributiondao.com/telegram/groups/link`,
+      method: `POST`,
+      contentType: "application/json",
+      headers: {
+        Authorization: `${token}`,
+      },
+      json: senddata,
+    };
+    request(options, (req, ress) => {
+      let data = ress.body ? ress.body : null;
+      res.status(200).send({ error: false, data });
+    });
+  }
+});
 module.exports = [router, detail];
